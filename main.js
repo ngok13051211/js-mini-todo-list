18e7311e90edca560cef299b0b401a3864581f93
ngok1 <ngok13051211@gmail.com>
2025-01-02 16:34:35 +0700

    3. Add New Tasks

--- main.js
+++ main.js
@@ -1,35 +1,52 @@
-const tasks = [
-  {
-    title: "Rua bat",
-    completed: true,
-  },
-  {
-    title: "Quet nha",
-    completed: false,
-  },
-  {
-    title: "Nau com",
-    completed: true,
-  },
-];
+const tasks = [];
 
 const taskList = document.querySelector("#task-list");
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const submitBtn = document.querySelector("#submit");
 
-const html = tasks
-  .map(
-    (task) => `
-    <li class="task-item">
-          <span class="task-title">${task.title}</span>
-          <div class="task-action">
-            <button class="task-btn edit">Edit</button>
-            <button class="task-btn done">${
-              task.completed ? "Mark as undone" : "Mark as done"
-            }</button>
-            <button class="task-btn delete">Delete</button>
-          </div>
-        </li>
-  `
-  )
-  .join("");
+submitBtn.onmousedown = function (e) {
+  e.preventDefault();
+};
 
-taskList.innerHTML = html;
+todoForm.onsubmit = function (e) {
+  e.preventDefault();
+
+  const value = todoInput.value.trim();
+
+  if (!value) {
+    return alert("Please write somethings");
+  }
+
+  const newTask = {
+    title: value,
+    completed: false,
+  };
+
+  tasks.unshift(newTask);
+
+  todoInput.value = "";
+  render();
+};
+
+function render() {
+  const html = tasks
+    .map(
+      (task) => `
+      <li class="task-item ${task.completed ? "completed" : ""}">
+            <span class="task-title">${task.title}</span>
+            <div class="task-action">
+              <button class="task-btn edit">Edit</button>
+              <button class="task-btn done">${
+                task.completed ? "Mark as undone" : "Mark as done"
+              }</button>
+              <button class="task-btn delete">Delete</button>
+            </div>
+          </li>
+    `
+    )
+    .join("");
+  taskList.innerHTML = html;
+}
+
+render();
