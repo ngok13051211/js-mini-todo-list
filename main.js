dad501f80cabc1d04b25a26ae2d8b7a8b8411446
ngok1 <ngok13051211@gmail.com>
2025-01-02 22:37:53 +0700

    4. Edit task

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-const tasks = [];
+const tasks = [
+  {
+    title: "rua chen",
+    completed: false,
+  },
+  {
+    title: "rua chen 1 ",
+    completed: true,
+  },
+  {
+    title: "rua chen 2",
+    completed: false,
+  },
+];
 
 const taskList = document.querySelector("#task-list");
 const todoForm = document.querySelector("#todo-form");
@@ -9,15 +22,14 @@ submitBtn.onmousedown = function (e) {
   e.preventDefault();
 };
 
+// Add Tasks
 todoForm.onsubmit = function (e) {
   e.preventDefault();
 
   const value = todoInput.value.trim();
-
   if (!value) {
     return alert("Please write somethings");
   }
-
   const newTask = {
     title: value,
     completed: false,
@@ -29,11 +41,30 @@ todoForm.onsubmit = function (e) {
   render();
 };
 
+// Edit
+taskList.onclick = function (e) {
+  const taskItem = e.target.closest(".task-item");
+  const taskIndex = +taskItem.getAttribute("task-index");
+  const task = tasks[taskIndex];
+
+  if (e.target.closest(".edit")) {
+    const newTitle = prompt("Enter the new task title: ", task.title);
+    task.title = newTitle;
+    render();
+  } else if (e.target.closest(".done")) {
+    console.log("Mark as done/undone");
+  } else if (e.target.closest(".delete")) {
+    console.log("Delete");
+  }
+};
+
 function render() {
   const html = tasks
     .map(
-      (task) => `
-      <li class="task-item ${task.completed ? "completed" : ""}">
+      (task, index) => `
+      <li class="task-item ${
+        task.completed ? "completed" : ""
+      }" task-index = "${index}">
             <span class="task-title">${task.title}</span>
             <div class="task-action">
               <button class="task-btn edit">Edit</button>
