e55a0bb13cf61de62878203d90906daa70ea1bd1
ngok1 <ngok13051211@gmail.com>
2025-01-03 17:17:35 +0700

    6. Fix bug message empty and Edit

--- main.js
+++ main.js
@@ -1,44 +1,37 @@
 const tasks = [
   {
-    title: "rua chen",
-    completed: false,
-  },
-  {
-    title: "rua chen 1 ",
-    completed: true,
-  },
-  {
-    title: "rua chen 2",
+    title: "Cong viec 1",
     completed: false,
   },
 ];
 
-const taskList = document.querySelector("#task-list");
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
+const taskList = document.querySelector("#task-list");
+
 const submitBtn = document.querySelector("#submit");
 
 submitBtn.onmousedown = function (e) {
   e.preventDefault();
 };
 
-// Add Tasks
 function addTasks(e) {
   e.preventDefault();
 
   const value = todoInput.value.trim();
+
   if (!value) {
-    return alert("Please write somethings");
+    return alert("Please write something!");
   }
+
   const newTask = {
     title: value,
     completed: false,
   };
 
-  tasks.unshift(newTask);
-
+  tasks.push(newTask);
   todoInput.value = "";
-  renderTasks();
+  renderTask();
 }
 
 // Edit
@@ -48,46 +41,57 @@ function handleTaskActions(e) {
   const task = tasks[taskIndex];
 
   if (e.target.closest(".edit")) {
-    const newTitle = prompt("Enter the new task title: ", task.title);
+    const newTitle = prompt("Enter the new task title", task.title);
+
+    if (newTitle === null) return;
+
+    if (!newTitle.trim()) {
+      return alert("Title cannot be empty");
+    }
+
     task.title = newTitle;
-    renderTasks();
+    renderTask();
   } else if (e.target.closest(".done")) {
     task.completed = !task.completed;
-    renderTasks();
+    renderTask();
   } else if (e.target.closest(".delete")) {
-    if (
-      confirm(
-        `Are you sure you want to delete task: ${task.title.toUpperCase()}?`
-      )
-    ) {
+    if (confirm("Are you sure you want to delete this task?")) {
       tasks.splice(taskIndex, 1);
-      renderTasks();
+      renderTask();
     }
   }
 }
 
-function renderTasks() {
+function emptyMessage(e) {}
+
+function renderTask() {
+  if (!tasks.length) {
+    taskList.innerHTML = `<li class="empty-message">No tasks available.</li>`;
+    return;
+  }
+
   const html = tasks
     .map(
       (task, index) => `
-      <li class="task-item ${
-        task.completed ? "completed" : ""
-      }" task-index = "${index}">
-            <span class="task-title">${task.title}</span>
-            <div class="task-action">
-              <button class="task-btn edit">Edit</button>
-              <button class="task-btn done">${
-                task.completed ? "Mark as undone" : "Mark as done"
-              }</button>
-              <button class="task-btn delete">Delete</button>
-            </div>
-          </li>
-    `
+    <li class="task-item ${
+      task.completed ? "completed" : ""
+    }" task-index = "${index}">
+    <span class="task-title">${task.title}</span>
+    <div class="task-action">
+        <button class="task-btn edit">Edit</button>
+        <button class="task-btn done">${
+          task.completed ? "Mark as done" : "Mark as undone"
+        }</button>
+        <button class="task-btn delete">Delete</button>
+    </div>
+    </li>
+`
     )
     .join("");
+
   taskList.innerHTML = html;
 }
 
 todoForm.addEventListener("submit", addTasks);
 taskList.addEventListener("click", handleTaskActions);
-renderTasks();
+renderTask();
